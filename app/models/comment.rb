89f9af86c4eed92ee8a4c8c5b1ae9a95fae7f571
TAEKEON GO <taekeongo@gmail.com>
2017-06-16 22:01:44 +0900

    build models and controllers and their relations. and Database is also completed.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :question
+
+  has_many :commentlikes
+  has_many :comment_likers, through :commentlikes, source :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :questions
+  has_many :comments
+  has_many :commentlikes
+  has_many :commented_questions, through :comments, source :question
+  has_many :liked_comments, through :commentlikes, source :comment
+
+  has_many :votes
+  has_many :photolikes
+  has_many :liked_photos, through :photolikes, source :photo
 end
