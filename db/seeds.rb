5454c846cbe27d8caac5d425ceffd1c56553a777
Wonhee Son <worhee421@gmail.com>
2017-06-23 07:42:04 +0000

    userphotos_temp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.create email: 'worhee421@likelion.org', password: "111111", name: "손원희", id: "1", status: '5'
-User.create email: 'coals1153@likelion.org', password: "111111", name: "임채민", id: "2", status: '5'
-User.create email: 'ktg2904@likelion.org', password: "111111", name: "고태건", id: "3", status: '5'
-User.create email: '07js23@likelion.org', password: "111111", name: "서재성", id: "4", status: '5'
-User.create email: 'kimtitu@likelion.org', password: "111111", name: "김유경", id: "5", status: '5'
-User.create email: 'alex1381@likelion.org', password: "111111", name: "김승현", id: "6", status: '5'
+User.create email: 'worhee421@likelion.org', password: "111111", name: "손원희", id: "1", status: '5', photo: 'http://snu.likelion.net/assets/%EA%B9%80%EA%B2%BD%EB%8F%84-b54be18ead6b8ad37a30bb1c2aeb40513ad7e3068c60c33ad8607c9298b0ffa9.png'
+User.create email: 'coals1153@likelion.org', password: "111111", name: "임채민", id: "2", status: '5', photo: 'http://snu.likelion.net/assets/%EA%B9%80%EA%B2%BD%EB%8F%84-b54be18ead6b8ad37a30bb1c2aeb40513ad7e3068c60c33ad8607c9298b0ffa9.png'
+User.create email: 'ktg2904@likelion.org', password: "111111", name: "고태건", id: "3", status: '5', photo: 'http://snu.likelion.net/assets/%EA%B9%80%EA%B2%BD%EB%8F%84-b54be18ead6b8ad37a30bb1c2aeb40513ad7e3068c60c33ad8607c9298b0ffa9.png'
+User.create email: '07js23@likelion.org', password: "111111", name: "서재성", id: "4", status: '5', photo: 'http://snu.likelion.net/assets/%EA%B9%80%EA%B2%BD%EB%8F%84-b54be18ead6b8ad37a30bb1c2aeb40513ad7e3068c60c33ad8607c9298b0ffa9.png'
+User.create email: 'kimtitu@likelion.org', password: "111111", name: "김유경", id: "5", status: '5', photo: 'http://snu.likelion.net/assets/%EA%B9%80%EA%B2%BD%EB%8F%84-b54be18ead6b8ad37a30bb1c2aeb40513ad7e3068c60c33ad8607c9298b0ffa9.png'
+User.create email: 'alex1381@likelion.org', password: "111111", name: "김승현", id: "6", status: '5', photo: 'http://snu.likelion.net/assets/%EA%B9%80%EA%B2%BD%EB%8F%84-b54be18ead6b8ad37a30bb1c2aeb40513ad7e3068c60c33ad8607c9298b0ffa9.png'
